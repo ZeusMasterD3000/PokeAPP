ecaec2cd1d1d74cce923353f56bd2ea7bcfb8902
ZeusMasterD3000 <snowfallk391@gmail.com>
2024-11-15 09:23:32 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,111 @@
+//Atributos poke rival
+const imgRival = document.querySelector('#pokeRival');
+const nombreRival = document.querySelector('#nombreRival');
+const tipo1Rival = document.querySelector('#tipo1Riva');
+const tipo2Rival = document.querySelector('#tipo2Rival');
+const atkFisRival = document.querySelector('#ataqueFisRival'); 
+const atkEspRival = document.querySelector('#ataqueEspRival');
+const vidaRival = document.querySelector('#vidaRival');
+const defensaEspRival = document.querySelector('#defensaEspRival');
+const defensaFisRival = document.querySelector('#defensaFisRival');
+const velocidadRival = document.querySelector('#velocidadRival');
+
+//Atributos poke propio
+const imgPropio = document.querySelector('#pokePropio');
+const nombrePropio = document.querySelector('#nombrePropio');
+const tipo1Propio = document.querySelector('#tipo1Propio');
+const tipo2Propio = document.querySelector('#tipo2Propio');
+const atkFisPropio = document.querySelector('#ataqueFisPropio'); 
+const atkEspPropio = document.querySelector('#ataqueEspPropio');
+const vidaPropio = document.querySelector('#vidaPropio');
+const defensaEspPropio = document.querySelector('#defensaEspPropio');
+const defensaFisPropio = document.querySelector('#defensaFisPropio');
+const velocidadPropio = document.querySelector('#velocidadPropio');
+
+//Interfaz de usuario
+
+const input = document.querySelector('#input');
+const btnElegir = document.querySelector('#btn-poke');
+const btnAtkFis  = document.querySelector('#btn-atk-fis');
+const btnAtkEsp  = document.querySelector('#btn-atk-esp');
+
+//Método de número random
+const getNumRandom = () => {
+    let min = Math.ceil(0);
+    let max = Math.floor(1001);
+
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+//Se elegirá un pokemon pero solo del tipo fantasma, el tipo de elección del pokemon queda a criterio del desarrollador, que sea divertido.
+const obtenerPokePropio = ()=>{
+    const num = input.value;
+
+    console.log(num);
+
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`).then((res)=>{
+        console.log(res.data)
+        return res.data
+    }).then((res)=>{
+        //console.log(res);
+        imgPropio.src = res.sprites.back_default;
+        nombrePropio.innerHTML = res.name;
+        tipo1Propio.innerHTML = res.types[0].type.name;
+        //tipo2Propio.innerHTML = res.types[1].type.name;
+        vidaPropio.innerHTML = res.stats[0].base_stat;
+        atkFisPropio.innerHTML = res.stats[1].base_stat;
+        defensaFisPropio.innerHTML = res.stats[2].base_stat;
+        atkEspPropio.innerHTML = res.stats[3].base_stat;
+        defensaEspPropio.innerHTML = res.stats[4].base_stat;
+        velocidadPropio.innerHTML = res.stats[5].base_stat;
+    })
+}
+//Se generará un pokemon rival aleatorio 
+const obtenerPokeRival = () =>{
+
+    const numPokeRival = getNumRandom();
+
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${numPokeRival}`).then((res)=>{
+       console.log(res.data)
+        return res.data
+    }).then((res)=>{
+        //console.log(res);
+        imgRival.src = res.sprites.front_default;
+        nombreRival.innerHTML = res.name;
+        tipo1Rival.innerHTML = res.types[0].type.name;
+        //tipo2Rival.innerHTML = res.types[1].type.name;
+
+       // if(res.types[1].type.name == undefined){ }
+
+        vidaRival.innerHTML = res.stats[0].base_stat;
+        atkFisRival.innerHTML = res.stats[1].base_stat;
+        defensaFisRival.innerHTML = res.stats[2].base_stat;
+        atkEspRival.innerHTML = res.stats[3].base_stat;
+        defensaEspRival.innerHTML = res.stats[4].base_stat;
+        velocidadRival.innerHTML = res.stats[5].base_stat;
+    })
+}
+//Combate, el pokemon perdedor será el que se le acabe primero su vida.
+//El usuario deberá elegir si ocupa ataque fisico o especial, según lo elegido los pokemon usarán su defensa especial o defensa fisica para bloquear los ataques
+//La defensa especial o fisica del pokemon que recibe el ataque sera restada del ataque especial o fisico del pokemon atacante, la diferencia será restada a la vida del pokemon defensor
+//En caso de que el resultado de la resta sea negativo o cero, se va a dejar un 1 como el resultado minimo de la resta
+//El pokemon que tenga más velocidad va a pegar primero
+//Se debe de aplicar la tabla de tipos al resultado de la resta de defensa y ataque, pero solo en daño, no en resitencias
+//Ejemplo poke1AtaqueFisico = 56;
+// poke2Defensafisica = 35; poke2vida = 98;
+// DañoRecibido = poke1AtaqueFisico - poke2DefensaFisica;
+//poke2VidaRestante = poke2Vida - DañoRecibido;
+//Se turnarán los pokemon hasta que haya un ganador
+//Mostrar el ganador
+const combate = ()=>{
+    
+    console.log('Estan peleando');
+}
+
+
+window.addEventListener('load', obtenerPokeRival);
+
+btnElegir.addEventListener('click', obtenerPokePropio);
+
+btnAtkFis.addEventListener('click',combate);
+
